139d4b2a0efcfb86767781731651bd02a95230d5
maggieclark <39098852+maggieclark@users.noreply.github.com>
2024-04-29 12:15:07 -0400

    use prevalidated array

--- lasso.R
+++ lasso.R
@@ -5,29 +5,6 @@ setwd('C:/Users/clark/Documents/GitHub/kaggle-abalone')
 
 imported = read_csv('train.csv')
 
-# define functions
-fold_ttsplit = function(prepped_data, outcome_column, fold){
-  
-  train  <- prepped_data %>% filter(fold_assn == fold) %>% select(!fold_assn)
-  print("train created")
-  test   <- prepped_data %>% filter(fold_assn != fold) %>% select(!fold_assn)
-  print('test created')
-  
-  Xtrain = train %>% 
-    select(!{{outcome_column}})
-  
-  ytrain = train %>% 
-    select({{outcome_column}})
-  
-  Xtest = test %>% 
-    select(!{{outcome_column}})
-  
-  ytest = test %>% 
-    select({{outcome_column}})
-  
-  return(list(Xtrain, ytrain, Xtest, ytest))
-}
-
 # drop ID and categorical sex
 data = imported %>% select(!c('id', 'Sex'))
 
@@ -38,34 +15,33 @@ cor(data)
 hist(data$Rings)
 
 # CV to determine best lambda
-ingredients = cv.glmnet(as.matrix(datasets[[1]]), 
-                        datasets[[2]]$Rings, 
+X = data %>% select(!Rings) %>% as.matrix()
+y = data %>% select(Rings) %>% as.matrix()
+
+# use truncated normal????????????????????????????
+ingredients = cv.glmnet(X, 
+                        y, 
                         family="poisson", 
                         standardize=TRUE,
                         keep = TRUE)
 
-ingredients$lambda.min
+# index of best lambda
+idx = ingredients$index[1]
 
-# calculate RMSLE
+# calculate kaggle-error
 
-# divide into 5 folds
-foldnames = c('fold1', 'fold2', 'fold3', 'fold4', 'fold5')
-fold_assn <- sample(foldnames, nrow(data), replace=TRUE)
-data = cbind(data, fold_assn)
-rm(fold_assn)
+# prevalidated array - contains y_hat from 10 folds using best lambda
+preval = cbind(y_hat = ingredients$fit.preval[,idx], 
+               fold = ingredients$foldid,
+               y = y) %>% 
+  as.data.frame() %>% 
+  mutate(kag_row_error = (log(1+y_hat) - log(1+y))^2)
 
-# CV loop for stable estimate
-for (f in 1:5){
-  print(f)
-  
-  foldname = foldnames[f]
-  
-  datasets = fold_ttsplit(data, 'Rings', foldname)
-  
-  fit1 = glmnet(datasets[[1]], datasets[[2]]$Rings, family="poisson", standardize=TRUE)
-  
-  X_test = datasets[[3]] %>% as.matrix()
-  y_hat = predict(fit1, X_test, type = "response")
-  
-  error = sqrt(mean((log(1 + y_hat) - log(1 + datasets[[4]]$Rings))^2))
+# loop through folds and calculated 10 kaggle-errors
+k_errors <- c()
 
+for (i in 1:10){
+  fold_dataset = preval %>% filter(fold == i) 
+  k_error = sqrt(mean(fold_dataset$kag_row_error))
+  k_errors = c(k_errors, k_error)
+}
